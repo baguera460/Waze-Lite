a5b74a8cd37699d1542c4e0f9ae36c3016b36d32
Juan Gomez <71553939+juan361@users.noreply.github.com>
2024-02-27 08:27:49 +0100

    Atlas : Ajout du fichier Slope.kt + modification de MainActivity

--- app/src/main/java/fr/isen/M1/Gomez/wazeliteski/MainActivity.kt
+++ app/src/main/java/fr/isen/M1/Gomez/wazeliteski/MainActivity.kt
@@ -3,16 +3,30 @@ package fr.isen.M1.Gomez.wazeliteski
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import fr.isen.M1.Gomez.wazeliteski.ui.theme.WazeLiteSkiTheme
+import com.google.firebase.database.*
+import androidx.compose.runtime.remember as remember
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity() {/*
+    val database = FirebaseDatabase.getInstance("https://waze-lite-ski-default-rtdb.europe-west1.firebasedatabase.app/")
+    var myRef = database.getReference("Slope")
+
+    var pisteData by remember { mutableStateOf("aucune donnée") }*/
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,26 +36,32 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
+                    Greeting()
+                    Spacer(modifier = Modifier.height(100.dp))
+
                 }
             }
+
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Greeting(modifier: Modifier = Modifier) {
+    Column {
+        Text(text = "Hello")
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(text = "Test")
+    }
 
-        modifier = modifier
-    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     WazeLiteSkiTheme {
-        Greeting("Android")
+        Greeting()
+
     }
 }
---
+++ app/src/main/java/fr/isen/M1/Gomez/wazeliteski/data/Slope.kt
@@ -0,0 +1,9 @@
+package fr.isen.M1.Gomez.wazeliteski.data
+
+import androidx.compose.ui.graphics.Color
+
+data class Slope(
+    val name: String = "",
+    val color : Number = 1,
+    var state : Boolean = true,
+)
