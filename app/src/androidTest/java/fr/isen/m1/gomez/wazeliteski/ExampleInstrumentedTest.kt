46ea94199b88c78488300a34fc2945a8ac402219
Juan Gomez <71553939+juan361@users.noreply.github.com>
2024-02-27 10:27:44 +0100

    uppercase

--- app/src/androidTest/java/fr/isen/m1/gomez/wazeliteski/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/fr/isen/m1/gomez/wazeliteski/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("fr.isen.M1.Gomez.wazeliteski", appContext.packageName)
+        assertEquals("fr.isen.m1.gomez.wazeliteski", appContext.packageName)
     }
 }
