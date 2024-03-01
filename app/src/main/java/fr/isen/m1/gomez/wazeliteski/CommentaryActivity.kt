9301f2c4de5b2414df8e34c85dd272d15ae75542
alex <you@example.com>
2024-03-01 15:48:19 +0100

    Ajout de l'activité CommentaryActivity

---
+++ app/src/main/java/fr/isen/m1/gomez/wazeliteski/CommentaryActivity.kt
@@ -0,0 +1,10 @@
+package fr.isen.m1.gomez.wazeliteski
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CommentaryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
