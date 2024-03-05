90f42cf582ec4f5249e55a8b4f6bb83bdf4eba18
Marc ETAVARD <rakushain@gmail.com>
2024-03-05 12:52:59 +0100

    Cas particuliers nuls, à refaire surement

--- app/src/main/java/fr/isen/m1/gomez/wazeliteski/SlopeLinkActivity.kt
+++ app/src/main/java/fr/isen/m1/gomez/wazeliteski/SlopeLinkActivity.kt
@@ -13,7 +13,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -25,6 +27,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.google.firebase.database.DataSnapshot
@@ -67,6 +70,7 @@ fun LinkView(slope: Slope?) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
+                .verticalScroll(rememberScrollState())
                 .background(Color(176, 196, 222))
         ) {
             Row(modifier = Modifier.offset(5.dp, 5.dp)) {
@@ -82,28 +86,41 @@ fun LinkView(slope: Slope?) {
                     fontSize = 20.sp
                 )
             }
-            Row(modifier = Modifier.padding(5.dp, 25.dp)) {
-                Text("Liste de toutes les pistes\n")
-            }
             val nexts = slope?.next
             if (nexts != null) {
-                for (i : Int in nexts)
+                for (i: Int in nexts)
                     GetNextSlopes(index = i, slopes)
             }
-            Text(slopes.toList().toString())
-            for (s : Slope in slopes)
-                TextButton(onClick = {
-                    val intent = Intent(context, SlopeLinkActivity::class.java)
-                    intent.putExtra(SlopeLinkActivity.SLOPE_EXTRA_KEY, s)
-                    context.startActivity(intent)
-                },
-                    modifier = Modifier.align(Alignment.CenterHorizontally))
+            if (slopes.isNotEmpty()) {
+                Row(modifier = Modifier
+                    .padding(5.dp, 25.dp)
+                    .align(Alignment.CenterHorizontally)) {
+                    Text("Pistes desservies \n",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold)
+                }
+                ParticularSlope(slope = slope)
+                for (s: Slope in slopes) {
+                    TextButton(
+                        onClick = {
+                            val intent = Intent(context, SlopeLinkActivity::class.java)
+                            intent.putExtra(SlopeLinkActivity.SLOPE_EXTRA_KEY, s)
+                            context.startActivity(intent)
+                        },
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    )
                     {
-                        Text(s.name)
+                        Text(s.name, color = (Level.from(s.color)).colorId())
                     }
+                }
 
+            }
 
-            Text(text = if (slope?.end == true) "Piste finale !" else "")
+            Text(
+                text = if (slope?.end == true && slope.next?.isEmpty() == null) "Piste finale !"
+                else if (slope?.end == true) "Possible de terminer la descente sur cette piste"
+                else ""
+            )
             Divider(modifier = Modifier.padding(0.dp, 15.dp), color = Color.Black)
             TextButton(
                 onClick = { /*TODO*/ },
@@ -120,12 +137,43 @@ fun LinkView(slope: Slope?) {
 
 
 @Composable
+fun ParticularSlope(slope: Slope?) {
+    if (slope?.name == "Le forest") {
+        Text(
+            "Attention ! \nSi vous provenez de la piste \"Bouticari vert\",\n" +
+                    "Vous arriverez à la fin de la piste \"Le forest\""
+        )
+    }
+    if (slope?.name == "Les jockeys")
+        Text(
+            "Attention ! \nSi vous provenez de la piste  \"Les chamois\",\n" +
+                    "Vous arriverez à la fin de la piste \"Les jockeys\""
+        )
+    if (slope?.name == "Pré méan")
+        Text(
+            "Attention ! \nSi vous provenez de la piste \"Le gourq\",\n" +
+                    "Vous arriverez à la fin de la piste \"Pré méan\""
+        )
+    if (slope?.name == "Le gourq")
+        Text(
+            "Attention ! \nSi vous provenez de la piste \"La mandarine\",\n" +
+                    "Vous ne pourrez pas rejoindre la piste \"La mandarine\""
+        )
+    if (slope?.name == "Les lampions")
+        Text(
+            "Attention ! \nSi vous provenez de la piste \"La mandarine\",\n" +
+                    "Vous ne pourrez rejoindre que les pistes \"Le S du chamois\","+
+                    "\" Les jockeys \" ou \" Le chamois \""
+        )
+}
+
+@Composable
 fun GetNextSlopes(index: Int?, slopes: MutableList<Slope>) {
     val ref = DataBaseHelper.database.getReference("slopes/$index")
     ref.addListenerForSingleValueEvent(object : ValueEventListener {
         override fun onDataChange(snapshot: DataSnapshot) {
             val slope = snapshot.getValue(Slope::class.java)
-            if (slope != null && !slopes.contains(slope)){
+            if (slope != null && !slopes.contains(slope)) {
                 println("Slope ${index}: color=${slope.color}, name=${slope.name}")
                 slopes.add(slope)
             }
